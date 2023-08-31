db1fdb4418ac0525d9535fed58f5fc8403a0ce0e
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-08-31 09:48:49 +0900

    Add:Global setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SWRConfig } from "swr/_internal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SWRConfig
+          value={{
+            revalidateOnFocus: false,
+            revalidateOnReconnect: false,
+            revalidateOnMount: false,
+          }}
+        >
+          {children}
+        </SWRConfig>
+      </body>
     </html>
   );
 }
