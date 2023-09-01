7d313031168562a918c1846c428cb343c02fd7e3
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-09-01 09:47:02 +0900

    Add:DisableAutomaticRevalidations

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import useSWR, { SWRConfiguration } from "swr";
+import useSWRImmutable from "swr/immutable";
 
 type RandomJoke = {
   id: number;
@@ -7,14 +8,16 @@ type RandomJoke = {
   punchline: string;
 };
 
+const RANDOM_JOKE_API_ENDPOINT =
+  "https://official-joke-api.appspot.com/random_joke";
+
 const fetcher = async (url: string) => {
   const res = await fetch(url);
   return await res.json();
 };
 
 export const useRandomJoke = (options: SWRConfiguration = {}) =>
-  useSWR<RandomJoke>(
-    "https://official-joke-api.appspot.com/random_joke",
-    fetcher,
-    options
-  );
+  useSWR<RandomJoke>(RANDOM_JOKE_API_ENDPOINT, fetcher, options);
+
+export const useRandomJokeImmutable = () =>
+  useSWRImmutable<RandomJoke>(RANDOM_JOKE_API_ENDPOINT, fetcher);
---
+++ src/app/auto-revalidation/disabled/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useRandomJokeImmutable } from "@/api";
+import { ROUTES_AUTO_REVALIDATION } from "../page";
+import { FetchedRandomJoke } from "../components/FetchedRandomJoke";
+
+export default function DisableAutomaticRevalidations() {
+  const heading = ROUTES_AUTO_REVALIDATION[4].label;
+  const { data, isValidating } = useRandomJokeImmutable();
+
+  return (
+    <FetchedRandomJoke
+      heading={heading}
+      description={`If the resource is immutable, that will never change if we revalidate again, we can disable all kinds of automatic revalidations for it.`}
+      isValidating={isValidating}
+      randomJoke={data}
+    />
+  );
+}
