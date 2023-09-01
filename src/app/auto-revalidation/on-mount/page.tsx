26069a27a9c25e9ea695113bd07acce25fa02bff
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-09-01 09:22:51 +0900

    Add:RevalidateOnMount

---
+++ src/app/auto-revalidation/on-mount/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useRandomJoke } from "@/api";
+import { ROUTES_AUTO_REVALIDATION } from "../page";
+import { FetchedRandomJoke } from "../components/FetchedRandomJoke";
+
+export default function RevalidateOnMount() {
+  const heading = ROUTES_AUTO_REVALIDATION[3].label;
+  const { data, isValidating } = useRandomJoke({ revalidateOnMount: true });
+
+  return (
+    <FetchedRandomJoke
+      heading={heading}
+      description={`It's useful to force override SWR revalidation on mounting.`}
+      isValidating={isValidating}
+      randomJoke={data}
+    />
+  );
+}
