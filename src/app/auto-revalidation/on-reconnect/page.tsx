571470ed8f3f0b2dc7a53a65b788052b18890edf
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-09-01 09:14:00 +0900

    Add:RevalidateOnReconnect

---
+++ src/app/auto-revalidation/on-reconnect/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useRandomJoke } from "@/api";
+import { ROUTES_AUTO_REVALIDATION } from "../page";
+import { FetchedRandomJoke } from "../components/FetchedRandomJoke";
+
+export default function RevalidateOnReconnect() {
+  const heading = ROUTES_AUTO_REVALIDATION[2].label;
+  const { data, isValidating } = useRandomJoke({ revalidateOnReconnect: true });
+
+  return (
+    <FetchedRandomJoke
+      heading={heading}
+      description={`It's useful to also revalidate when the user is back online. This scenario happens a lot when the user unlocks their computer, but the internet is not yet connected at the same moment.`}
+      isValidating={isValidating}
+      randomJoke={data}
+    />
+  );
+}
