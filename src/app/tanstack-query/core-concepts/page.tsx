838d82bec13f64cb2d855aa2ff505206c2b41c0c
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-02-28 10:20:20 +0900

    Add:description

--- src/app/tanstack-query/core-concepts/page.tsx
+++ src/app/tanstack-query/core-concepts/page.tsx
@@ -15,7 +15,7 @@ export default function CoreConcepts() {
   return (
     <RandomJokeContainer
       heading={heading}
-      description=""
+      description="TanStack Query (FKA React Query) is often described as the missing data-fetching library for web applications, but in more technical terms, it makes fetching, caching, synchronizing and updating server state in your web applications a breeze."
       isValidating={isPending}
       errorMessage={error?.message}
       randomJoke={data}
